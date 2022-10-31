96318a5cac14af5b2daf08e56e6e8bfaf71efa03
237035848 <237035848@qq.com>
2022-10-31 13:43:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -60,7 +60,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU16917TVkiBmRzhYiU9HPv8V8VJIq6EOazcDAd0',
+      id: 'PDU16917TLh2FVQWO4xZeJrVyeHtqESZ1A1U5keXN',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
